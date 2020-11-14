43d4cbb0c99dc69555843211101b417e6113e5b2
mehtavarun <mehtavarun96@rediffmail.com>
2020-11-14 15:12:46 +0530

    inital working code

---
+++ src/main/java/com/nagarro/nagp/big/data/config/ConfigProperties.java
@@ -0,0 +1,21 @@
+package com.nagarro.nagp.big.data.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+
+@Getter
+@ConfigurationProperties(prefix = "")
+public class ConfigProperties {
+
+    @Value("${spark.app-name}")
+    private String sparkAppName;
+
+    @Value("${file.path:#{systemProperties.get('user.dir')}}")
+    private String filePath;
+
+    @Value("${file.name}")
+    private String filename;
+
+}
