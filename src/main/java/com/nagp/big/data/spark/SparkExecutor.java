c3872054f710174c72adcf44f61838df03c50cac
mehtavarun <mehtavarun96@rediffmail.com>
2020-11-22 15:58:29 +0530

    docs added

--- src/main/java/com/nagp/big/data/spark/SparkExecutor.java
+++ src/main/java/com/nagp/big/data/spark/SparkExecutor.java
@@ -1,10 +1,7 @@
 package com.nagp.big.data.spark;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -18,6 +15,7 @@ import java.util.concurrent.Future;
 import javax.annotation.PreDestroy;
 
 import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
@@ -54,13 +52,25 @@ public class SparkExecutor implements ApplicationRunner {
 
     private static final String BIG_DATA_TOPIC = "nagp.bigdata";
 
+    /**
+     * reads the csv file and creates a temporay view from it and writes a parquet file of it
+     * 
+     * @throws FileNotFoundException
+     */
     private void run() throws FileNotFoundException {
         Dataset<Invoice> invoicesCsv = encodeToBean(Invoice.class, getCsv());
         invoicesCsv.createOrReplaceTempView("Inv");
         writeToParquet(invoicesCsv, "invoices.parquet");
     }
 
-    private void getInvoicesWithInvAmntGtThenInvAmnt(String outputParquetName)
+    /**
+     * gets invoices with invoiceAmount greater than average invoices amount for vendor
+     * 
+     * @param outputParquetName
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    private void getInvoicesWithInvAmntGtThanAvgInvAmntForVendor(String outputParquetName)
             throws InterruptedException, ExecutionException {
         Dataset<Row> invoicesRowData = spark.sql(
                 "SELECT * FROM Inv I1 WHERE I1.InvoiceTotal > (SELECT AVG(I2.InvoiceTotal) FROM INV I2 WHERE I1.InvoiceVendorName = I2.InvoiceVendorName)");
@@ -69,6 +79,13 @@ public class SparkExecutor implements ApplicationRunner {
                 () -> saveDataInCassandra(invoiceData));
     }
 
+    /**
+     * gets the count of invoices with date difference by 1 Year between required columns
+     * 
+     * @param outputParquetName
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
     private void getCountInvWithDateDiffBy1Y(String outputParquetName) throws InterruptedException, ExecutionException {
         Dataset<Row> invoiceParquet = readParquet("invoices.parquet");
         Dataset<Invoice> invoicesDateDiffGt1 = encodeToBean(Invoice.class, findInvoicesDateDiffGt1(invoiceParquet));
@@ -76,6 +93,13 @@ public class SparkExecutor implements ApplicationRunner {
                 () -> sendData(invoicesDateDiffGt1), () -> saveDataInCassandra(invoicesDateDiffGt1));
     }
 
+    /**
+     * executes various runnable methods with multithreading using executorService
+     * 
+     * @param runnables
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
     private void processInvoiceData(Runnable... runnables) throws InterruptedException, ExecutionException {
         List<Future<?>> futures = new LinkedList<>();
         for (Runnable runnable : runnables) {
@@ -86,36 +110,57 @@ public class SparkExecutor implements ApplicationRunner {
         }
     }
 
+    /**
+     * encodes the dataset type to the beanClass provided
+     * 
+     * @param <T>
+     * @param <R>
+     * @param beanClass
+     * @param dataSet
+     * @return dataSet encoded to a particular object
+     */
+    private static <T, R> Dataset<R> encodeToBean(Class<R> beanClass, Dataset<T> dataSet) {
+        return dataSet.as(Encoders.bean(beanClass));
+    }
+
+    /**
+     * changes the formats of dates of type string given in csv to a particular format(MM-dd-yyyy)
+     * 
+     * @param invoices
+     * @return Dataset for invoices with dates formatted to correct format to be read by spark
+     */
     private Dataset<Row> findInvoicesDateDiffGt1(Dataset<Row> invoices) {
         return invoices.toDF()
                 .filter(functions
-                        .months_between(functions.when(functions
-                                .to_date(functions.col("InvoiceRecvdDate"), "MM/dd/yyyy").isNotNull(),
-                                functions.to_date(functions.col("InvoiceRecvdDate"), "MM/dd/yyyy"))
-                                .otherwise(functions.when(functions
-                                        .to_date(functions.col("InvoiceRecvdDate"), "M/d/yyyy").isNotNull(),
-                                        functions.to_date(functions.col("InvoiceRecvdDate"), "M/d/yyyy"))
-                                        .otherwise(functions.when(
-                                                functions.to_date(functions.col("InvoiceRecvdDate"), "MM-dd-yyyy")
-                                                        .isNotNull(),
-                                                functions.to_date(functions.col("InvoiceRecvdDate"), "MM-dd-yyyy")))),
-                                functions
-                                        .when(functions.to_date(functions.col("InvoiceDate"), "MM/dd/yyyy")
-                                                .isNotNull(),
-                                                functions.to_date(functions.col("InvoiceDate"), "MM/dd/yyyy"))
-                                        .otherwise(functions
-                                                .when(functions.to_date(functions.col("InvoiceDate"),
-                                                        "M/d/yyyy").isNotNull(),
-                                                        functions.to_date(functions.col("InvoiceDate"), "M/d/yyyy"))
-                                                .otherwise(functions.when(
-                                                        functions.to_date(functions.col("InvoiceDate"), "MM-dd-yyyy")
-                                                                .isNotNull(),
-                                                        functions.to_date(functions.col("InvoiceDate"),
-                                                                "MM-dd-yyyy")))),
+                        .months_between(
+                                getDateWithFormatIfExists("InvoiceRecvdDate", "MM/dd/yyyy")
+                                        .otherwise(getDateWithFormatIfExists("InvoiceRecvdDate", "M/d/yyyy").otherwise(
+                                                getDateWithFormatIfExists("InvoiceRecvdDate", "MM-dd-yyyy"))),
+                                getDateWithFormatIfExists("InvoiceDate", "MM/dd/yyyy")
+                                        .otherwise(getDateWithFormatIfExists("InvoiceDate", "M/d/yyyy")
+                                                .otherwise(getDateWithFormatIfExists("InvoiceDate", "MM-dd-yyyy"))),
                                 true)
                         .divide(12).gt(1.00));
     }
 
+    /**
+     * creates a conditional function which checks if the date in that column is of given format type. If of given type
+     * then returns the value otherwise null
+     * 
+     * @param columnName
+     * @param format
+     * @return Column
+     */
+    private Column getDateWithFormatIfExists(String columnName, String format) {
+        return functions.when(functions.to_date(functions.col(columnName), format).isNotNull(),
+                functions.to_date(functions.col(columnName), format));
+    }
+
+    /**
+     * saves the data in cassandra for given dataset
+     * 
+     * @param dataset
+     */
     private void saveDataInCassandra(Dataset<Invoice> dataset) {
         Map<String, String> map = new HashMap<>();
         map.put("keyspace", "nagp");
@@ -125,11 +170,21 @@ public class SparkExecutor implements ApplicationRunner {
                 .mode(SaveMode.Append).options(map).save();
     }
 
+    /**
+     * publishes the kafka messages for given dataset
+     * 
+     * @param dataSet
+     */
     private void sendData(Dataset<Invoice> dataSet) {
         dataSet.collectAsList().forEach(invoice -> messagePublisher.sendMessage(BIG_DATA_TOPIC, invoice.toString(),
                 UUID.randomUUID().toString()));
     }
 
+    /**
+     * reads the csv file for the path set in properties. some options are preset for easiness
+     * 
+     * @return Dataset for csv file read
+     */
     private Dataset<Row> getCsv() {
         String filepath = properties.getFilePath() + File.separator + properties.getFilename();
         Dataset<Row> invoicesCsv = spark.read().format("csv").option("header", "true").load(filepath)
@@ -138,43 +193,39 @@ public class SparkExecutor implements ApplicationRunner {
         return invoicesCsv;
     }
 
-    private static <T, R> Dataset<R> encodeToBean(Class<R> beanClass, Dataset<T> dataSet) {
-        return dataSet.as(Encoders.bean(beanClass));
-    }
-
+    /**
+     * reads the parquet file for given name
+     * 
+     * @param parquetName
+     * @return Dataset of parquet file read
+     */
     private Dataset<Row> readParquet(String parquetName) {
         return spark.read().parquet(parquetName);
     }
 
+    /**
+     * Writes dataset to parquet file
+     * 
+     * @param invoicesData
+     * @param filename
+     */
     private void writeToParquet(Dataset<Invoice> invoicesData, String filename) {
         invoicesData.write().format("parquet").mode(SaveMode.Overwrite)
                 .save(properties.getFilePath() + File.separator + filename);
     }
 
-    private void performUserQuery() throws IOException {
-//        findInvoicesDateDiffGt1(testParquet);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Dataset<Invoice> testParquet = readParquet("invoices.parquet").as(Encoders.bean(Invoice.class));
-        testParquet.createOrReplaceTempView("Inv");
-//        while (true) {
-//            try {
-//                spark.sql(br.readLine()).show();
-//            } catch (Exception e) {
-//                System.err.println(e.getMessage());
-//            }
-//            System.out.println();
-//        }
-        sendData(testParquet);
-        saveDataInCassandra(testParquet);
-    }
-
+    /**
+     * Method: run() is a dependency for other functions for first time application runs as it creates invoices.parquet
+     * file which other methods reads. One can comment any of following methods and run only a single method at a time
+     * or multiples
+     */
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // this method is required to create invoices.parquet which will be further used by next questions to read and
+        // process data
         run();
-//        performUserQuery();
-        getInvoicesWithInvAmntGtThenInvAmnt("invoices_with_amnt_gt_invtotal.parquet");
+        getInvoicesWithInvAmntGtThanAvgInvAmntForVendor("invoices_with_amnt_gt_invtotal.parquet");
         getCountInvWithDateDiffBy1Y("date_diff_gt_by_1.parquet");
-//        getTop5HighestTurnoverVendors();
         System.exit(1);
     }
 
